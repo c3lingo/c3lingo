4ae2f47fa0f678a1dc61deac7b33f185553e116c
Uriel Corfa <korfuri@gmail.com>
2020-04-27 17:21:08 +0200

    Add initial Django models
    These models are ported from the specs at
    https://hackmd.io/uG6Wnj4bRcGIkB4ljZzf7g with a few naming changes for
    consistency. The only significant change is that Rooms are now a model
    instead of being referred to by name.

--- c3lingo/admin.py
+++ c3lingo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Language, Conference, Room, Talk, Translation, Translator, TranslatorSpeaks, Booth, Shift, ShiftAssignment
 
-# Register your models here.
+admin.site.register(Language)
+admin.site.register(Conference)
+admin.site.register(Room)
+admin.site.register(Talk)
+admin.site.register(Translation)
+admin.site.register(Translator)
+admin.site.register(TranslatorSpeaks)
+admin.site.register(Booth)
+admin.site.register(Shift)
+admin.site.register(ShiftAssignment)
